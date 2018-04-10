7bf4c0a9dde07beb657f8c79b2c5624d76edb042
Joachim Hofer <jmhofer.github@johoop.de>
2018-04-10 23:15:20 +0200

    implement start of an agent trait, and also epsilon-greedy for tic-tac-toe

--- examples/tictactoe/src/main.rs
+++ examples/tictactoe/src/main.rs
@@ -1,9 +1,12 @@
+extern crate rand;
 extern crate rl;
 
 mod tictactoe;
 
 use rl::*;
 use tictactoe::*;
+use rand::{Rng, thread_rng};
+use std::collections::HashMap;
 
 struct TicTacToeEnv {}
 
@@ -15,16 +18,51 @@ impl Environment for TicTacToeEnv {
         Game::new()
     }
 
-    fn actions(state: &Game) -> Vec<usize> {
-        state.actions()
+    fn actions(game: &Game) -> Vec<usize> {
+        game.actions()
     }
 
-    fn take_action(state: &Game, action: &usize) -> (Game, f64) {
-        (state.take_action(*action), 0.0) // TODO compute and return a proper reward
+    fn take_action(game: &Game, action: &usize) -> (Game, f64) {
+        (game.take_action(*action), 0.0) // TODO compute and return a proper reward
     }
 
-    fn render(state: &Game) {
-        println!("{}", state.render());
+    fn render(game: &Game) {
+        println!("{}", game.render());
+    }
+}
+
+const EPSILON: f64 = 0.1;
+
+struct TicTacToeAgent {
+    v: HashMap<u16, f64>
+}
+
+impl Agent<Game, usize> for TicTacToeAgent {
+    fn get_q(&self, game: Game, action: &usize) -> f64 {
+        *self.v.get(&game.take_action(*action).hash()).unwrap_or(&0.0)
+    }
+
+    fn update_q(&mut self, game: Game, action: &usize, value: f64) {
+        self.v.insert(game.take_action(*action).hash(), value);
+    }
+
+    // epsilon-greedy policy
+    fn get_policy(&self, game: Game, actions: &[usize]) -> usize {
+        let mut rng = thread_rng();
+        if rng.gen::<f64>() < EPSILON {
+            *rng.choose(actions).unwrap()
+        } else {
+            let mut chosen = actions[0];
+            let mut max_q = self.get_q(game, &chosen);
+            for action in actions[1..].iter() {
+                let q = self.get_q(game, action);
+                if q > max_q {
+                    max_q = q;
+                    chosen = *action;
+                }
+            }
+            chosen
+        }
     }
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -7,13 +7,22 @@ mod tests {
 }
 
 pub trait Environment {
-    type Action: Eq;
-    type State: Eq;
+    type Action;
+    type State;
 
     fn new() -> Self::State;
 
     fn actions(state: &Self::State) -> Vec<Self::Action>;
     fn take_action(state: &Self::State, action: &Self::Action) -> (Self::State, f64);
 
-    fn render(state: &Self::State) {}
+    fn render(_state: &Self::State) {}
+}
+
+pub trait Agent<State, Action> {
+
+    fn get_q(&self, state: State, action: &Action) -> f64;
+    fn update_q(&mut self, state: State, action: &Action, value: f64);
+
+    fn get_policy(&self, state: State, actions: &[Action]) -> Action;
+    // TODO updating the policy
 }
